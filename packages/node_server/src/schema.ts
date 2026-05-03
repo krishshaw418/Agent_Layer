32e82452da5a619b40b9e848f6fba5dbb72926bf
Krish Shaw <shawkrish418@gmail.com>
2026-05-03 15:34:54 +0530

    fix: add redis_url to default.config.json for setting up node

--- packages/node_server/src/schema.ts
+++ packages/node_server/src/schema.ts
@@ -1,7 +1,6 @@
 import z from "zod";
 
 export const manualConfigSchema = z.object({
-    UPSTASH_REDIS_URL: z.string(),
     NODE_QUERY_URL: z.string(),
     NODE_API_KEY: z.string(),
     BASE_RPC_URL: z.string(),
@@ -12,6 +11,7 @@ export const manualConfigSchema = z.object({
 
 export const defaultConfigSchema = z.object({
     PORT: z.coerce.number().default(3000),
+    UPSTASH_REDIS_URL: z.string(),
     CONTRACT_ADDRESS: z.string(),
     NEW_JOBS_CHANNEL: z.string(),
     JOB_ASSIGN_CHANNEL: z.string(),
